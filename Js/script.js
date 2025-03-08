25f26d3537aad298fe0b55ce57791c0826db43b6
Albi Luzi <albi.luzi.al@outlook.com>
2025-03-08 15:07:54 +0100

    Changes to script

--- Js/script.js
+++ Js/script.js
@@ -7,6 +7,20 @@ function returnRomanNR(lat romanNR){
     if(romanNR == "X"){
         return 10 }
         if(romanNR == "L"){
-            return 50 }   
+            return 50 }  
+            
+            return 8888888
+}
 
+for(var i = 0; i<=array.length;i++)
+    { var test=0
+               if(!Number(array[i])){
+
+                for(var j =0;j<=array[i].length;j++){
+              test +=  returnRomanNR(array[i][j]);
+                }
+               if(test == 8888888){
+                console.log("Numer i pa kthyeshem")
+               }
+               }
 }
