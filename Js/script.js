34b524cfa628773f7ec714d6423c2ce9f9894027
Albi Luzi <albi.luzi.al@outlook.com>
2025-03-08 15:00:49 +0100

    Changes to JS

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+let array = [1,2,"X",8,"XII"]
+ 
+
+function returnRomanNR(lat romanNR){
+    if(romanNR == "I"){
+    return 1}
+    if(romanNR == "X"){
+        return 10 }
+        if(romanNR == "L"){
+            return 50 }   
+
+}
